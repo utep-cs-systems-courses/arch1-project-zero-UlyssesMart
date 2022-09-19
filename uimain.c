358d5710c4819fe60442ebca27e9cef4840f1d06
student <umartinez2@miners.utep.edu>
2022-09-19 10:41:33 -0600

    Finished lab 0, had errors pushing

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c, Arrow = a) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c, Arrow = a, Chars 5x7 = d) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -26,8 +26,13 @@ int main()
       break;
     case 'c':
       puts("You selected chars:");
-      for (char c = 'A'; c < 'D'; c++)
+      for (char c = 'A'; c < 'H'; c++)
 	print_char_11x16(c);
+      break;
+    case 'd':
+      puts("You selected 5x7 chars");
+      for(char c = 'B'; c < 'K'; c++)
+	print_char_5x7(c);
       break;
     case 'a': //New switch case i implemented myself
       puts("You selected an arrow:");
